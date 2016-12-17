a7b70e82fd761abfc3f58575ebcb0df845eacc3c
ksinn <ksinn@mail.ru>
2016-12-17 21:19:13 +0500

    last last

--- API/src/API/AppInf.java
+++ API/src/API/AppInf.java
@@ -11,8 +11,8 @@ package API;
  * @author ksinn
  */
 public class AppInf {
-    public static String a = "http://localhost:8084";
-            //"http://tomcat.cybercloud.uz";
+    public static String a = //"http://localhost:8084";
+            "http://tomcat.cybercloud.uz";
     public static String main =  a+"/elearning";
     public static String task =  a+"/task-modul";
     public static String HMACSHA256Key =  "elearning-main_12345";
